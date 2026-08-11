de022d33c7493bc289ffdc5c7400acbf1aa12c88
Vojtěch Kadlec <clemensmayr726@gmail.com>
2026-08-11 16:14:27 +0200

    Optimize custom logger implementation

---
+++ logger.py
@@ -0,0 +1,30 @@
+import logging
+
+# Configure the logger
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+class CustomLogger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+# Example usage
+if __name__ == '__main__':
+    logger = CustomLogger(__name__)
+    logger.info('This is an info message')
+    logger.error('This is an error message')
