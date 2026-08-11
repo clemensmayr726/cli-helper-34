4d1d6e058db8ab516af0f9ce8318e91549c46138
Vojtěch Kadlec <clemensmayr726@gmail.com>
2026-08-11 16:16:12 +0200

    Refactor config loading process

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+class Config:
+    def __init__(self, config_file='config.json'):
+        self.config_file = config_file
+        self.settings = self.load_config()
+
+    def load_config(self):
+        if not os.path.exists(self.config_file):
+            raise FileNotFoundError(f'Config file not found: {self.config_file}')
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
+
+    def get(self, key, default=None):
+        return self.settings.get(key, default)
+
+    def set(self, key, value):
+        self.settings[key] = value
+        self.save_config()
+
+    def save_config(self):
+        with open(self.config_file, 'w') as file:
+            json.dump(self.settings, file, indent=4)
